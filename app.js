f5ea0ef6ee70190f7dafdc5deaba8df5af08a19e
Bharat Mungalpara <bharatmungalpara664@gmail.com>
2023-09-10 14:57:24 +0530

    features and reservation section done

--- app.js
+++ app.js
@@ -1,9 +1,42 @@
 const toggle_menu = document.querySelector(".toggle_ul");
 const open = document.querySelector(".three_line")
 const close = document.querySelector(".close_line")
+
 function toggle_open(){
 
   toggle_menu.classList.toggle("open");
   open.classList.toggle("close")
   close.classList.toggle("open")
 }
+
+
+const counter_section = document.querySelector(".counter_sec");
+const ob = new IntersectionObserver((enter,exit)=>{
+  const [entry]=enter;
+  if(!entry.isIntersecting){
+    return;
+  }
+  else{
+    const dispval = document.querySelectorAll(".numbers");
+    let interval = 4000;
+    dispval.forEach((dispval)=>{
+      let start= 0;
+      let end = parseInt(dispval.getAttribute("data"));
+      let duration = Math.floor(interval/end);
+      let counter = setInterval(function(){
+        start += 1;
+        dispval.textContent = start;
+        if(start==end)
+        {
+          clearInterval(counter);
+        }
+      },duration);
+    });
+  }
+
+  exit.unobserve(counter_section)
+},{
+  root:null,
+  threshold:0,
+});
+ob.observe(counter_section);
