a71b981d366a4d7f82c561172b91227a1ee8474c
Bharat Mungalpara <bharatmungalpara664@gmail.com>
2023-09-21 21:04:59 +0530

    full project completed

---
+++ components/footer.php
@@ -0,0 +1,39 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="images/email-icon.png" alt="">
+         <h3>our email</h3>
+         <a href="mailto:shaikhanas@gmail.com">bharatmungalpara664@gamil.com</a>
+         <a href="mailto:anasbhai@gmail.com">jayrupapara2003@gmail.com</a>
+      </div>
+
+      <div class="box">
+         <img src="images/clock-icon.png" alt="">
+         <h3>opening hours</h3>
+         <p>09:00am to 09:00pm</p>
+      </div>
+
+      <div class="box">
+         <img src="images/map-icon.png" alt="">
+         <h3>our address</h3>
+         <a href="#">Rajkot, Gujarat - 360003</a>
+      </div>
+
+      <div class="box">
+         <img src="images/phone-icon.png" alt="">
+         <h3>our number</h3>
+         <a href="tel:+91 7698963943">+91 7698963943</a>
+         <a href="tel:+91 9313240006">+91 9313240006</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>Y-NOT FOODS</span> | all rights reserved!</div>
+
+</footer>
+
+<div class="loader">
+   <img src="images/loader.gif" alt="">
+</div>
